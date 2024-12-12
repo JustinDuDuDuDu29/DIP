f4e67cad409081064188798b1023e7940bc1c3c8
Wang Yi An <crayon715@gmail.com>
2024-12-12 19:21:52 +0800

    123

---
+++ yolov11/yolo.py
@@ -0,0 +1,25 @@
+import os
+import glob
+
+from ultralytics import YOLO
+
+
+def main(folder_path : str):
+
+        image_folder = glob.glob(os.path.join(folder_path + "*.png"))
+        print(len(image_folder))
+
+        # for i in range(len(image_folder)):
+        #         model = YOLO("./model/yolo11x.pt")
+        #         result = model.predict(
+        #                 source=image_folder[i],
+        #                 mode="predict",
+        #                 save=True,
+        #                 device="cpu"
+        #         )
+
+        print("done")
+
+if __name__ == "__main__":
+        folder_path = "../enhance_nk/"
+        main(folder_path)
