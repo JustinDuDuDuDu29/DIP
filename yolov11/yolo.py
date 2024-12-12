a131640c085b1fd16b5a919d7ca112b0d0458a19
Wang Yi An <crayon715@gmail.com>
2024-12-12 21:49:32 +0800

    clean again

--- yolov11/yolo.py
+++ yolov11/yolo.py
@@ -9,14 +9,14 @@ def main(folder_path : str):
         image_folder = glob.glob(os.path.join(folder_path + "*.png"))
         print(len(image_folder))
 
-        # for i in range(len(image_folder)):
-        #         model = YOLO("./model/yolo11x.pt")
-        #         result = model.predict(
-        #                 source=image_folder[i],
-        #                 mode="predict",
-        #                 save=True,
-        #                 device="cpu"
-        #         )
+        for i in range(len(image_folder)):
+                model = YOLO("./model/yolo11x.pt")
+                result = model.predict(
+                        source=image_folder[i],
+                        mode="predict",
+                        save=True,
+                        device="cpu"
+                )
 
         print("done")
 
